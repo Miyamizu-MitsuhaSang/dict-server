be92e1d754b8f657cae3a393cce17e6d1eddc11f
Miyamizu-MitsuhaSang <2510681107@qq.com>
2025-08-17 16:27:53 +0800

    更新的init文件提升跨模块访问

---
+++ app/models/__init__.py
@@ -0,0 +1,4 @@
+from . import signals
+from .fr import WordlistFr, DefinitionFr, AttachmentFr
+from .jp import WordlistJp, DefinitionJp, AttachmentJp
+from base import User
---
+++ app/utils/__init__.py
@@ -0,0 +1,1 @@
+from .security import redis_client
