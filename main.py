94f46857c6a1cf05aa77e986e1aa3fc388961c5b
Miyamizu-MitsuhaSang <2510681107@qq.com>
2025-11-06 20:06:40 +0800

    搜索次数接口

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from app.api.util_api.routes import ulit_router
 from app.api.word_comment.routes import word_comment_router
 from app.core.redis import init_redis, close_redis
 from app.utils.phone_encrypt import PhoneEncrypt
-from settings import TORTOISE_ORM
+from settings import ONLINE_SETTINGS
 
 
 @asynccontextmanager
@@ -47,7 +47,7 @@ app.add_middleware(
 
 register_tortoise(
     app=app,
-    config=TORTOISE_ORM
+    config=ONLINE_SETTINGS,
 )
 
 app.include_router(users_router, tags=["User API"], prefix="/users")
