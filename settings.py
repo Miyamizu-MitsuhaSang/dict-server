eb5c922c90fca87772aa095d321ec0c0e1741b8d
Miyamizu-MitsuhaSang <2510681107@qq.com>
2026-06-26 02:00:04 +0800

    修复数据库连接

--- settings.py
+++ settings.py
@@ -32,7 +32,7 @@ TORTOISE_ORM = {
 
 ONLINE_SETTINGS = {
     'connections': {
-        'default': 'mysql://root:@127.0.0.1:3306/test_db',
+        'default': 'mysql://kingqin:enterprise927@127.0.0.1:3306/test_db',
     },
     'apps': {
         'models': {
